2d9135ca449bfc912fe3be9261df0cd465460f17
Vladimir_Voronin <v.voronin0204@gmail.com>
2024-12-06 03:20:27 +0700

    Added scene transition interactable component

---
+++ Mauseu_project/Assets/Scripts/TradeMarket/Characrer/Interaction/Components/InteractableSceneComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TradeMarket.Characrer
+{
+    public class InteractableSceneComponent : InteractableItemComponentBase
+    {
+        [SerializeField] private string _sceneName;
+        //add scene manager
+        
+        public override void Interract()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
